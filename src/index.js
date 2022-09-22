b858c611cc860c661a8b471e4d8a8dfa6b1af0bd
AegisVP <vlad@pysarenko.com>
2022-09-22 23:59:30 +0200

    initial copy

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import theme from 'components/Common/Theme.jsx';
+import { ThemeProvider } from 'styled-components';
+import { App } from 'components/App/App';
+import { theme } from './components/Common/Theme';
 import './index.css';
 
-///////////////////
-// Маршрутизация //
-///////////////////
-//
-// Если приложение использует библиотеку react-router-dom для маршрутизации,
-// необходимо дополнительно настроить компонент < BrowserRouter >,
-// передав в пропе basename точное название твоего репозитория.
-// Слеши в начале и конце строки обязательны.
-//
-// <BrowserRouter basename="/goit-react-hw-06-phonebook/">
-//   <App />
-// </BrowserRouter>
-//
-///////////////////
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
       <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    </React.StrictMode>
+  </ThemeProvider>
 );
