35bc59664745be6e877fb31aeb3d8dce8c7a95b4
AegisVP <vlad@pysarenko.com>
2022-09-28 00:26:00 +0200

    redux toolkit slice

--- src/redux/filter/actions.js
+++
@@ -1,7 +0,0 @@
-// import { createAction } from '@reduxjs/toolkit';
-
-// export const searchFilter = createAction('filter/search', search => {
-//   return {
-//     payload: { search },
-//   };
-// });
